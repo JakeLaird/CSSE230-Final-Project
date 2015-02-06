85d33d3baeca416563d02a58e007eca8791c7a61
JakeLaird <laird.jacob@gmail.com>
2015-02-06 18:19:51 -0500

    Map Point class

---
+++ src/MapPoint.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+
+/**
+ * 
+ */
+
+/**
+ * @author Jake
+ *
+ */
+public class MapPoint {
+	double latitude,longitude,cost;
+	PriorityQueue<MapPoint> neighboringPoints;
+	MapPoint(double latitude, double longitude){
+		this.latitude = latitude;
+		this.longitude = longitude;	
+		
+	}
+	
+}
