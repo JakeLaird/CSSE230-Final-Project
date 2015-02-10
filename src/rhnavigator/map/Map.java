f35ab42052fdaa7c6f8fe1df0d19f3be0ab31757
Tayler <tayler.burns@gmail.com>
2015-02-10 09:26:02 -0500

    - Started adding the KD tree to look into the map

--- src/rhnavigator/map/Map.java
+++ src/rhnavigator/map/Map.java
@@ -1,6 +1,7 @@
 package rhnavigator.map;
 import java.util.ArrayList;
 import rhnavigator.MapPoint;
+import net.sf.javaml.core.kdtree.KDTree;
 /**
  * Stores the all of the MapPoints in the currently loaded map
  */
@@ -12,15 +13,18 @@ import rhnavigator.MapPoint;
 
 public class Map {
 	private ArrayList<MapPoint> mapPoints;
+	private KDTree mapPointsTree;
 
   public Map() {
     mapPoints = new ArrayList<MapPoint>();
+    mapPointsTree = new KDTree(2); // 2 dimensions for our application
   }
 
   public Boolean addPoint(double latitude, double longitude, 
                           String name, ArrayList<String> neighbors,
                           int interestLevel) {
     MapPoint newPoint = new MapPoint(latitude, longitude);
+    mapPointsTree.insert(new double[] {latitude, longitude}, newPoint);
     throw new UnsupportedOperationException();
   }
 }
