3ed1c60283db52fbde512bcd494fcdade9b77c91
Tayler <tayler.burns@gmail.com>
2015-02-11 16:01:54 -0500

    - Updated MapView to use Swing, not awt

--- src/rhnavigator/map/MapView.java
+++ src/rhnavigator/map/MapView.java
@@ -1,7 +1,9 @@
 package rhnavigator.map;
 
-import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
+
+import javax.swing.JComponent;
 
 /**
  * A view into a specific Map
@@ -11,14 +13,19 @@ import java.awt.Graphics;
  * @author Tayler
  */
 
-public class MapView extends Canvas {
+public class MapView extends JComponent {
 	private Map map;
 
 	public MapView(Map map) {
 		this.map = map;
 	}
 
-	public void paint(Graphics g) {
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(0,0,getWidth(),getHeight());
+		System.out.println("W " + getWidth() + " H " + getHeight());
+		g.setColor(Color.BLACK);
 		g.drawOval(10, 10, 5, 8);
 	}
 }
